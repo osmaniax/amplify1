a2c03e8299040849c6a702ed5684ce956341731c
akimitsu.murata@gmail.com <muratti>
2020-11-21 08:32:12 +0900

    From Atom

--- hoge.js
+++ hoge.js
@@ -1,3 +1,5 @@
 document.write("Hello,Javascript!!");
 document.write("<br>");
 document.writeln("Hello,JavaScript20201120 From VSCode!!!");
+document.write("<br>");
+document.writeln("Hello,JavaScript20201121 From VSCode!!!");
