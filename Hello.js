298c1d31c7470ab31846ca7fe6c528f10f3986f1
akimitsu.murata@gmail.com <muratti>
2020-11-21 08:22:37 +0900

    From VSCode

--- Hello.js
+++ Hello.js
@@ -1,15 +1,15 @@
-var i=0;
-while (i<10){
-        process.stdout.write("Hello,World!!\n");
+var i = 0;
+while (i < 10) {
+        process.stdout.write("Hello,World!!\n") ;
         i++;
 }
-var j=0;
-do{
-        process.stdout.write("do Hello,World!!\n");
+var j = 0;
+do {
+        process.stdout.write("do Hello,World!!\n") ;
         j++;
-}while(j<10);
-for(let k=0;k<10;k++){
-        process.stdout.write("for Hello,World!!\n");
+} while (j < 10);
+for (let k=0; k < 10;k++) {
+        process.stdout.write("for Hello,World!!\n") ;
 }
 
 //#const fs = require('fs');
